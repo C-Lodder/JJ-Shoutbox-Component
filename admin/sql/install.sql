0ddaeb1830f04ccbe56867454d38cdfe1f820f54
Charlie.lodder <charlie.lodder@mointernational.com>
2015-03-23 16:22:33 +0000

    added SQL to to add smilies table

---
+++ admin/sql/install.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `#__shoutbox_smilies` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `code` varchar(12) NOT NULL DEFAULT '',
+  `image` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+INSERT INTO `#__shoutbox_smilies` (`id`, `code`, `location`) VALUES
+(1, ':)', 'icon_e_smile.gif'),
+(2, ':(', 'icon_e_sad.gif'),
+(3, ':D', 'icon_e_biggrin.gif'),
+(4, 'xD', 'icon_e_biggrin.gif'),
+(5, ':p', 'icon_razz.gif'),
+(6, ':P', 'icon_razz.gif'),
+(7, ';)', 'icon_e_wink.gif'),
+(8, ':S', 'icon_e_confused.gif'),
+(9, ':@', 'icon_mad.gif'),
+(10, ':O', 'icon_e_surprised.gif'),
+(11, 'lol', 'icon_lol.gif');
